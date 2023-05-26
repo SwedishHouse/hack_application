761144ff1e22f0dbf11fc89f14f459a1a646e808
Вадим Мещеряков <swedish2015@yandex.ru>
2023-05-26 19:42:12 +0400

    init

---
+++ app/src/main/java/com/example/hackapp/StoreFragment.kt
@@ -0,0 +1,20 @@
+package com.example.hackapp
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class StoreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_store, container, false)
+    }
+
+    // остальной код фрагмента
+}
